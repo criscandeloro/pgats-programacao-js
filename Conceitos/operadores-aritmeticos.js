89cfea050ecde4b4bcc90e2d1c2108455d15a29e
criscandeloro <criscandeloro@gmail.com>
2025-05-02 15:29:22 -0300

    conceitos operadores aritmeticos

---
+++ Conceitos/operadores-aritmeticos.js
@@ -0,0 +1,27 @@
+/*
++ adição
+- subtração
+* multiplicação
+ / divisão
+% resto
+** exponencial
+*/
+
+console.log("adição")
+console.log(`1 + 1 = ${1 + 1}`) // 2
+
+console.log("subtração")
+console.log(`10 - 7 = ${10 - 7}`) // 3
+
+console.log("multiplicação")
+console.log(`2 * 2 = ${2 * 2}`) // 4
+
+console.log("divisão")
+console.log(`11 / 2 = ${11 / 2}`) // 5.5
+
+console.log("resto")
+console.log(`11 % 2 = ${11 % 2}`) // 1
+
+console.log("exponencial")
+console.log(`4 ** 4 = ${4 ** 4}`) //256
+
