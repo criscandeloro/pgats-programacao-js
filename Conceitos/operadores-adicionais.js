5e41e1c9255b69f9573f7bde8e514e797ec7a11c
criscandeloro <criscandeloro@gmail.com>
2025-05-02 16:02:56 -0300

    Conceitos de operatores adicionais

---
+++ Conceitos/operadores-adicionais.js
@@ -0,0 +1,22 @@
+/**
+ -- decremento
+ ++ incremento
+ ? ternário
+ */
+
+ console.log("Incremento")
+
+ let pesoDoDog = 10
+
+ console.log(pesoDoDog)
+ pesoDoDog++
+ console.log(pesoDoDog)
+ 
+ console.log("Decremento")
+
+ let pesoDoTutor = 10
+ 
+ console.log(pesoDoTutor)
+ pesoDoTutor--
+ console.log(pesoDoTutor)
+
