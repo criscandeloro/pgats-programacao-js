e887825a50fb8fe3611796e3bca6624615761d71
criscandeloro <criscandeloro@gmail.com>
2025-05-03 08:12:51 -0300

    condicionais swith case

---
+++ Conceitos/condicionais-switch-case.js
@@ -0,0 +1,22 @@
+/**
+  escolha (x)
+  caso A
+  caso B
+  caso C
+ */
+ const peso = 12
+
+  switch(peso){
+    case 10:
+    console.log('P')
+    break
+    case 20 :
+    console.log('M')
+    break
+    case 30:
+    console.log('G')
+    break
+    default:
+    console.log('ND')    
+
+  }
