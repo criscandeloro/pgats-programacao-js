2fdca94fa0043e8fad1b24ef8e97856f8fc81beb
criscandeloro <criscandeloro@gmail.com>
2025-05-03 07:19:15 -0300

    Funcoes tradicionais

---
+++ Conceitos/funcoes-tradicionais.js
@@ -0,0 +1,44 @@
+
+//funcao sem retorno, com parametro
+function saudacoes(nome){
+    return `Olá, ${nome}!`
+}
+
+console.log(saudacoes('Teste2'))
+
+//função nomeada com parametro e com retorno
+function obterNomeDoDogFormatado(nome){
+    return nome.toUpperCase()
+}
+
+console.log(obterNomeDoDogFormatado('Pipoca'))
+
+//funcao nomeada com parametro e com default
+function exibirPorteDoDog(porte = 'ND'){
+    console.log(porte)
+}
+
+exibirPorteDoDog()
+
+//function com retorno uma array
+function obterObjetoDog(){
+    return [{
+        nome: 'Cris',
+        peso: 12
+    }, {
+        nome: 'Lázaro',
+        peso: 15
+    }]
+}
+
+console.log(obterObjetoDog())
+
+//fuction com retorno de uma matriz
+function listarNomeDosDogs(){
+    return [
+        'Cris',
+        'Marea',
+        'Fiat Uno'
+    ]
+}
+console.log(listarNomeDosDogs())
