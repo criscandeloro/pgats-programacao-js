faad047e4300747ac85c01def4d53205eff948ce
criscandeloro <criscandeloro@gmail.com>
2025-05-03 08:44:58 -0300

    Exercicio porte do Dog usando ternario

---
+++ Exercicios/05-exercicio-condicionais.js
@@ -0,0 +1,23 @@
+porteDoDog('Thor', 5.5)
+porteDoDog('Cris', 21)
+
+/*const porteDog = pesoDoDogTernario <= 10
+                 ? 'pequeno'                
+                 : 'medio'
+                 
+
+
+ console.log(`Nome do Dog: Thor - Peso do Dog: ${pesoDoDogTernario} - Porte do Dog: ${porteDog}`)
+*/
+
+function porteDoDog(nome, peso){
+    const porteDog = peso <= 10
+    ? 'pequeno'                
+    : 'medio'
+    
+
+
+console.log(`Nome do Dog: ${nome} - Peso do Dog: ${peso} - Porte do Dog: ${porteDog}`)
+
+
+}
