8f61b6487302b794a2bfa11b57fe1940ab4b5bec
criscandeloro <criscandeloro@gmail.com>
2025-05-03 07:59:36 -0300

    condicionais if else

---
+++ Conceitos/condicionais-if-else.js
@@ -0,0 +1,25 @@
+/**
+ condicao verdadeira
+ acao dentro do if
+ senao 
+ acao dentro do else
+ senao se 
+ acao dentro do senao se
+ */
+
+if (10 < 5) {
+    console.log('Valor é maior')
+} else {
+        console.log('Valor é menor')
+    }
+
+    const idade = 3
+    const porte = 'P'
+
+    if(idade<=2){
+        console.log('Pode ser adotado')
+    } else if(porte === 'P'){
+        console.log('Pode ser adotado pelo porte')
+    } else {
+        console.log('Não pode ser adotado')
+    }
