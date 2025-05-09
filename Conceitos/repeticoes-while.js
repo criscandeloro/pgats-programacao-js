c7938ec37a1dbcdde239fb5e94b66d2ad05d682c
criscandeloro <criscandeloro@gmail.com>
2025-05-09 20:08:12 -0300

    while

---
+++ Conceitos/repeticoes-while.js
@@ -0,0 +1,27 @@
+/**
+ Estrutura de repetição
+ enquanto(o pet nao tiver cansado); faca {
+ dar uma volta na quadra}
+ */
+
+ let voltasAteFicarCansado = 5
+ let quantidadeVoltasAtual = 0
+
+ while(quantidadeVoltasAtual < voltasAteFicarCansado){
+    console.log(`Dar uma volta na quadra`)
+
+    //momento de break da condicional
+    quantidadeVoltasAtual++
+
+ }
+
+ // execucao do código
+
+//over engineering
+
+//boilerplate
+//templates
+
+// template playwright + cucumber github
+
+ 
