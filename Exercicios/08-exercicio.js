f2cad33cfbab1479a73ced373f04887cb0c51d4c
criscandeloro <criscandeloro@gmail.com>
2025-05-10 09:46:56 -0300

    exercicio 08

---
+++ Exercicios/08-exercicio.js
@@ -0,0 +1,45 @@
+const qtdPetiscosDisponiveis = 4
+
+
+for (let petisco = 1; petisco <= qtdPetiscosDisponiveis; petisco++) {
+    console.log(`Petisco entregue ${petisco}`)
+
+}
+console.log("O Dog está satisfeito!")
+
+
+//função
+/*
+qtdPetiscosDisponiveis()
+
+function qtdPetiscosDisponiveis() {
+    const qtdPetiscosDisponiveis = 4
+
+
+    for (let petisco = 1; petisco <= qtdPetiscosDisponiveis; petisco++) {
+        console.log(`Petisco entregue ${petisco}`)
+
+    }
+    console.log("O Dog está satisfeito!")
+}
+*/
+
+/*while(qtdDogPetisco<=qtdPetiscos){
+    
+    console.log(`Petisco entregue ${qtdDogPetisco}`)
+    qtdDogPetisco++
+}
+ console.log("O Dog está satisfeito!")
+/*
+qtdPetiscosEntregue(2)
+
+ function qtdPetiscosEntregue(qtdPetiscos){
+    let qtdDogPetisco = 1
+    while(qtdDogPetisco<=qtdPetiscos){
+    
+    console.log(`Petisco entregue ${qtdDogPetisco}`)
+    qtdDogPetisco++
+}
+ console.log("O Dog está satisfeito!")
+
+ }*/
