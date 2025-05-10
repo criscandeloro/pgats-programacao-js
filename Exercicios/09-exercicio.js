3955fc9a1a1b4ccde0f15dbe45d8599d757d3dd5
criscandeloro <criscandeloro@gmail.com>
2025-05-10 10:13:03 -0300

    exercicio 09

---
+++ Exercicios/09-exercicio.js
@@ -0,0 +1,23 @@
+const brinquedos = ['Bola','Osso','Corda', 'Sino']
+/*
+listaDeBrinquedosDog.forEach(brinquedo => 
+    {
+
+        console.log(`Brinquedo entregue ${brinquedo}`)
+
+    }
+)*/
+
+ brinquedosFunction()
+
+function brinquedosFunction(){
+
+brinquedos.forEach(brinquedo /*conteudo da posição na lista*/ => 
+    {
+
+        console.log(`Brinquedo entregue ${brinquedo}`)
+
+    }
+)
+}
+
